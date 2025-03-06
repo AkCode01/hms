38ee82ecc639e43e8f50d9983bef5ebf3047b7df
Akcode01 <arif.kashmiree@gmail.com>
2025-03-06 10:48:29 +0500

    Bug Fixes after live to hostinger

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\PostController;
-use App\Http\Controllers\Api\HospitalController;
+use App\Http\Controllers\API\HospitalController;
 // use App\Models\Post;
 // use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +16,8 @@ Route::middleware('auth:sanctum')->group(function(){
   
     Route::apiResource('posts', PostController::class);
   
-    Route::get('/hospitals/', [HospitalController::class, 'index']); 
-    Route::post('/hospitals/', [HospitalController::class, 'store']); 
+    Route::get('/hospitals', [HospitalController::class, 'index']); 
+    Route::post('/hospitals', [HospitalController::class, 'store']); 
     Route::get('/hospitals/{id}', [HospitalController::class, 'show']); 
     Route::put('/hospitals/{id}', [HospitalController::class, 'update']); 
     Route::delete('/hospitals/{id}', [HospitalController::class, 'destroy']); 
